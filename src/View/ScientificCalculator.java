734ac63dbbcd44be07e67bfe5b554f977708d9e5
ghimani614 <60162829+ghimani614@users.noreply.github.com>
2021-07-28 15:44:40 -0400

    MVC architecture created

---
+++ src/View/ScientificCalculator.java
@@ -0,0 +1,48 @@
+package View;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+
+import Model.ScientificCalculatorComponents;
+
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class ScientificCalculator {
+
+	//private JFrame frame;
+	//private JTextField textFieldResult;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ScientificCalculatorComponents window = new ScientificCalculatorComponents();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	
+	public ScientificCalculator() {
+		
+	}
+		
+}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+
