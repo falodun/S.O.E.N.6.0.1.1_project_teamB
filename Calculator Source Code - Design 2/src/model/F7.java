27467552d6bd94630790f82409c6b4c68f968c1b
ghimani614 <60162829+ghimani614@users.noreply.github.com>
2021-08-06 14:23:16 -0400

    functionality for F7 updated

--- Calculator Source Code - Design 2/src/model/F7.java
+++ Calculator Source Code - Design 2/src/model/F7.java
@@ -7,7 +7,7 @@ public class F7 extends AbstractFunction {
     String selectedFunctionMessage = "SELECTED FUNCTION: " + functionName + "\n\n";
     double x = 0;
     double y = 0;
-    double result = 0;
+    double result = 1;
 
     @Override
     public void getInputs() {
@@ -25,7 +25,50 @@ public class F7 extends AbstractFunction {
 
     @Override
     public void calculateResult() {
-        result = x*y; //todo: replace with real result after function's implementation
+        		//CASE 1: negative base values with fraction powers
+				if(x < 0 && y % 1 != 0) {
+					System.out.println("Error: Negative base can not have non-integer powers");
+				}else {
+					
+					//CASE 2: Power is a positive integer
+					if(y % 1 == 0 && y > 0) { 
+						for(int i = 1; i <= y; i++) {
+							result *= x;
+						}
+					//CASE 3: Power is a negative integer
+					}else if(y < 0 && y % 1 == 0) { 
+					
+						x = 1 / x;
+						y = -1 * y;
+						
+						for(int i = 1; i <= y; i++) {
+							result *= x;
+						}
+					//CASE 4: Power is a fraction e.g. square root, cube root etc.	
+					}else if(y % 1 != 0) { 
+					
+						if(y < 0) {
+							x = 1 / x;
+							y = -1 * y;
+							
+							double root = findRoot(x, y);
+							
+							//formatting up to 5 decimal places
+							DecimalFormat df = new DecimalFormat("#.#####");  
+							root = Double.parseDouble(df.format(root));
+							result = root;
+						
+						}else if(powerValue >= 0) {
+							double root = findRoot(x, y);
+							
+							//formatting up to 5 decimal places
+							DecimalFormat df = new DecimalFormat("#.#####");  
+							root = Double.parseDouble(df.format(root));
+							result = root;	
+						}
+					}
+				}
+       
     }
 
 }
