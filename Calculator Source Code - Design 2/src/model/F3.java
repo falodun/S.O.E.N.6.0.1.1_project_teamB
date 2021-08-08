7a91591850dff1c3cab57d8c50a12d06e0f75970
Adekunle Falodun <32306667+falodun@users.noreply.github.com>
2021-08-07 21:26:40 -0400

    Added F3 implementation

--- Calculator Source Code - Design 2/src/model/F3.java
+++ Calculator Source Code - Design 2/src/model/F3.java
@@ -2,6 +2,8 @@ package model;
 import view.FunctionInputField;
 import view.FunctionResultView;
 
+import static constants.Constants.EULER;
+
 public class F3 extends AbstractFunction {
     public String functionName = "sinh(x)";
     double x = 0;
@@ -21,7 +23,113 @@ public class F3 extends AbstractFunction {
 
     @Override
     public void calculateResult() {
-        result = x*2; //todo: replace with real result after function's implementation
+        result = sinH(x);
+    }
+
+    public double sinH(double x) {
+        double eulerRaisedToX = raiseBaseToExponent(EULER, x);
+        return (eulerRaisedToX-(1/eulerRaisedToX))/2;
+    }
+
+    public double raiseBaseToExponent(double base, double exponent) {
+        double result = 1;
+        boolean isNegativeExponent = false;
+
+        if (exponent == 0) {
+            return 1;
+        }
+
+        if (exponent < 0) {
+            exponent *= -1;
+            isNegativeExponent = true;
+        }
+
+        if (base < 0 && exponent%1 > 0) {
+            return Double.NaN;
+        }
+
+        if (base == 1) {
+            return 1;
+        }
+
+        double wholePart = (long) exponent;
+        double decimalPart = exponent%1;
+
+        double remainingWhole = wholePart;
+        double nextWhole;
+        double copy;
+        boolean firstLoop = true;
+        double baseRaisedToNextTenthExponent = base;
+
+        do {
+            copy = remainingWhole;
+            remainingWhole /= 10;
+            nextWhole = copy%10;
+
+            if (!firstLoop) {
+                baseRaisedToNextTenthExponent = raise(baseRaisedToNextTenthExponent, 10);
+            }
+            result *= raise(baseRaisedToNextTenthExponent, nextWhole);
+            firstLoop = false;
+        } while (remainingWhole > 0);
+
+        double remainingDecimal = decimalPart;
+        double nextDecimal;
+        double baseRootedToNextTenthRoot = base;
+
+
+        for (long i = 1; i <= 20; i++) {
+            copy = remainingDecimal;
+            remainingDecimal = (remainingDecimal%0.1)*10;
+            nextDecimal = (long) (copy/0.1);
+            baseRootedToNextTenthRoot = nthRoot(baseRootedToNextTenthRoot, 10);
+            result *= raise(baseRootedToNextTenthRoot, nextDecimal);
+        }
+
+
+        return isNegativeExponent ? 1/result : result;
+    }
+
+    private double raise(double base, double exponent) {
+        double result = 1;
+        if (exponent == 0) {
+            return 1;
+        }
+        for (long i = 1; i <= exponent; i++) {
+            result *= base;
+        }
+        return result;
+    }
+
+    public double nthRoot(double number, long n) {
+        double result = 0;
+
+        if (number == 0) {
+            return 0;
+        }
+
+        double precision = 0.000000000000001;
+        double oldGuess;
+        double guess = 0;
+        boolean endLoop = false;
+        long index = 0;
+        while (!endLoop) {
+            oldGuess = guess;
+            guess = precision*raise(10, index);
+            if (raise(guess + result, n) < number) {
+                index++;
+            }
+            else {
+                if (index == 0) {
+                    endLoop = true;
+                }
+                else {
+                    result += oldGuess;
+                    index = 0;
+                }
+            }
+        }
+        return result;
     }
 
 }
