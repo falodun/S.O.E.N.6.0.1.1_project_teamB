7f06823f63414f79e11c9da792c37c4ee076ec9d
Natt <Natt@VAIO>
2021-08-09 23:36:16 -0400

    Adding unit tests for F5

--- Calculator Source Code - Design 2/src/tests/F5Tests.java
+++ Calculator Source Code - Design 2/src/tests/F5Tests.java
@@ -1,14 +1,55 @@
 package tests;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
+import model.F5;
+
+/**
+ * 
+ * @author Natheepan Ganeshamoorthy (Natt)
+ *
+ */
 public class F5Tests {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testNotEqualZeroForVariableA() {
+		F5 f5 = new F5();
+		f5.a = 0;
+		f5.b = 2;
+		f5.x = 3;
+		f5.calculateResult();
+        assertEquals("ERROR: INVALID INPUT. Parameter 'a' cannot be 0.", f5.errorMsg);
+	}
+	
+	@Test
+	public void testNotEqualZeroForVariableB() {
+		F5 f5 = new F5();
+		f5.a = 2;
+		f5.b = 0;
+		f5.x = 3;
+		f5.calculateResult();
+        assertEquals("ERROR: INVALID INPUT. Parameter 'b' must be greater than 0, and cannot be 1.", f5.errorMsg);
+	}
+	
+	@Test
+	public void testNotEqualOneForVariableB() {
+		F5 f5 = new F5();
+		f5.a = 2;
+		f5.b = 1;
+		f5.x = 3;
+		f5.calculateResult();
+        assertEquals("ERROR: INVALID INPUT. Parameter 'b' must be greater than 0, and cannot be 1.", f5.errorMsg);
+	}
+	
+	@Test
+	public void testNotLessThanZeroForVariableB() {
+		F5 f5 = new F5();
+		f5.a = 2;
+		f5.b = -2;
+		f5.x = 3;
+		f5.calculateResult();
+        assertEquals("ERROR: INVALID INPUT. Parameter 'b' must be greater than 0, and cannot be 1.", f5.errorMsg);
 	}
 
 }
